a4687bfe1cd1486c7813a8c77fbeb2a3352394f6
Naman Arora <namanarora1022@gmail.com>
2024-07-14 22:28:45 +0530

    feat: arrow head

--- src/lib/draw.ts
+++ src/lib/draw.ts
@@ -1,15 +1,36 @@
 import { P5CanvasInstance } from '@p5-wrapper/react';
 import Point from './point';
 
+export const drawLine = (p5: P5CanvasInstance, p1: Point, p2: Point) => {
+    p5.line(p1.x, -p1.y, p2.x, -p2.y);
+};
+
+const drawArrowhead = (p5: P5CanvasInstance, base: Point, vec: Point) => {
+    const arrowSize = 7;
+    const angle = Math.atan2(vec.y, vec.x);
+
+    // Coordinates for the arrowhead
+    const arrowX1 = base.x - arrowSize * Math.cos(angle - Math.PI / 6);
+    const arrowY1 = base.y - arrowSize * Math.sin(angle - Math.PI / 6);
+    const arrowX2 = base.x - arrowSize * Math.cos(angle + Math.PI / 6);
+    const arrowY2 = base.y - arrowSize * Math.sin(angle + Math.PI / 6);
+
+    p5.line(base.x, -base.y, arrowX1, -arrowY1);
+    p5.line(base.x, -base.y, arrowX2, -arrowY2);
+};
+
 export const drawVector = (
     p5: P5CanvasInstance,
     p1: Point,
     p2: Point
 ): void => {
     drawLine(p5, p1, p2);
-    // TODO: draw arrow head
-};
 
-export const drawLine = (p5: P5CanvasInstance, p1: Point, p2: Point) => {
-    p5.line(p1.x, -p1.y, p2.x, -p2.y);
+    // Vector direction
+    const vector = {
+        x: p2.x - p1.x,
+        y: p2.y - p1.y
+    };
+
+    drawArrowhead(p5, p2, vector);
 };
