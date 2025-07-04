121ae268b15d980c9ed96a951bbdfd833992cff6
Naman Arora <namanarora1022@gmail.com>
2025-07-04 14:24:58 +0530

    feat: clean

--- src/interfaces/index.ts
+++
@@ -1,8 +0,0 @@
-import { SketchProps } from '@p5-wrapper/react';
-
-export type GraphingProps = {
-    expression: string;
-    graph: boolean;
-};
-
-export type MySketchProps = SketchProps & GraphingProps;
